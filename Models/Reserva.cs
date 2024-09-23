a17172d7752d4036eaf5fb49709e35858243e565
Vitor Takami <vtakami@unicamp.br>
2024-09-23 13:19:27 -0300

    Commit inicial

---
+++ Models/Reserva.cs
@@ -0,0 +1,59 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI*
+            if (Suite.Capacidade >= Hospedes.Count())
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new ArgumentException("Quantidade de hóspedes excedeu a capacidade da Suíte");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            int valor=Hospedes.Count();
+            Console.WriteLine($"A quantidade de hóspedes é de : {valor}");
+            return 0;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            decimal valor = 0;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (true)
+            {
+                valor = 0;
+            }
+
+            return valor;
+        }
+    }
+}
